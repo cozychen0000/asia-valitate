96c7b02a9712752e0064c7e1b177a1d57dc3dc8d
cozy <cozychen@liontravel.com>
2024-10-01 17:03:53 +0800

    fix: 處理小數點格式化

--- src/utils/price-format.ts
+++ src/utils/price-format.ts
@@ -14,11 +14,37 @@ export function priceFormat(value: string): string {
 
   const number = Number(validInput);
 
-  //如果有小數點，不做格式化
+  //如果有小數點，前面格式化，後面維持
   if (validInput.includes(".")) {
-    return validInput;
+    const [integer, decimal] = validInput.split(".");
+    return Number(integer).toLocaleString() + "." + decimal;
   }
 
   //千分位符號
   return number.toLocaleString();
 }
+
+
+// function addComma(val:string) {
+//   const [integer, decimal] = val.toString().split('.');
+//   const integerWithComma = integer.replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+//   return decimal ? `${integerWithComma}.${decimal}` : integerWithComma;
+// }
+
+// function formatPrice(val:string) {
+//   // 保留開頭的加號或減號
+//   if (!val || val === '-' || val === '+') return '0';
+//   if (val.startsWith('0.')) return val;
+//   return val.replace(/^0+/, '') || '0';
+// }
+
+// function removeComma(num: number | string): string {
+//   return num.toString().replace(/,/g, '');
+// }
+
+// function handleChange(val : string) {
+//   const raw = removeComma(val);
+//   const sanitizedVal = formatPrice(raw);
+//   if (Number.isNaN(Number(sanitizedVal))) return;
+//   setPriceVal(addComma(sanitizedVal));
+// }
